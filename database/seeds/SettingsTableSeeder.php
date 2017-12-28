7c0601479bbd9b570eb3cabfc583efa0adf94244
Wiwat Srisattha <wiwat_srt@hotmail.com>
2017-12-28 18:25:59 +0700

    added support timestamps

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -15,12 +15,18 @@ class SettingsTableSeeder extends Seeder
             [
                 'setting_key' => 'headerTitleText',
                 'setting_value' => 'Laravel 5 Starter',
+				'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
             ], [
                 'setting_key' => 'dateFormat',
                 'setting_value' => 'd/m/Y',
+				'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
             ], [
                 'setting_key' => 'resultsPerPage',
                 'setting_value' => '15',
+				'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
             ],
         ]);
     }
